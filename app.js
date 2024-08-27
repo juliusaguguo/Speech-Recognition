40ba738f4d313edbbf4cdea2c8c719a88c2bd48c
Julius Aguguo <juliusaguguo@gmail.com>
2024-08-28 00:19:31 +0100

    sending the github

---
+++ app.js
@@ -0,0 +1,14 @@
+let element = document.getElementById('text');
+
+const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition);
+
+recognition.lang = "en-GB";
+recognition.continuous = true;
+
+document.onclick = recognition.start();
+
+recognition.onresult = (event) => {
+    for (const result of event.results){
+        element.innerText = result[0].transcript;
+    }
+}
